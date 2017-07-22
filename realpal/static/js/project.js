76a4ea51afa0d220e06ab819c9e87e689ad479bb
Harish Maiya <harish.maiya@workday.com>
2017-07-22 02:40:17 -0700

    Changes in title, wording and renamed few fields

--- realpal/static/js/project.js
+++ realpal/static/js/project.js
@@ -39,20 +39,20 @@ $(document).ready(function () {
        $('.prepare a').children('.works').stop().slideUp()
    });
 
-   $('.header .row .buy').mouseover(function(){
-       $('.buy a').children('.works').stop().slideDown()
+   $('.header .row .purchase').mouseover(function(){
+       $('.purchase a').children('.works').stop().slideDown()
    });
 
-    $('.header .row .buy').mouseout(function(){
-       $('.buy a').children('.works').stop().slideUp()
+    $('.header .row .purchase').mouseout(function(){
+       $('.purchase a').children('.works').stop().slideUp()
    });
 
-   $('.header .row .live').mouseover(function(){
-       $('.live a').children('.works').stop().slideDown()
+   $('.header .row .own').mouseover(function(){
+       $('.own a').children('.works').stop().slideDown()
    });
 
-    $('.header .row .live').mouseout(function(){
-       $('.live a').children('.works').stop().slideUp()
+    $('.header .row .own').mouseout(function(){
+       $('.own a').children('.works').stop().slideUp()
    });
 
 
