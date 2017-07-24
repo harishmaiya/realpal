b0783f4696844628810f40d45720692052a61ac0
Udit <udit.wal@gmail.com>
2017-07-24 22:17:07 +0530

    Added URL points for static pages and their templates; Added models on Profile

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
@@ -15,8 +14,8 @@ urlpatterns = [
     # User management
     url(r'^users/', include('realpal.users.urls', namespace='users')),
     url(r'^accounts/', include('allauth.urls')),
-
     # Your stuff: custom urls includes go here
+    url(r'^', include('realpal.mainapp.urls'), name='mainapp'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- realpal/users/models.py
+++ realpal/users/models.py
@@ -5,12 +5,64 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
+class City(models.Model):
+    name = models.CharField(max_length=60)
+    state = models.CharField(max_length=40)
+
+
 @python_2_unicode_compatible
 class User(AbstractUser):
-
+    LANGUAGE_CHOICES = (
+        ('EN', 'English'),
+        ('ESP', 'Spanish'),
+        ('CA', 'Mandarin/Cantonese'),
+        ('HI', 'Hindi'),
+        ('OT', 'Other'),
+    )
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    language = models.CharField(
+        max_length=2, choices=LANGUAGE_CHOICES, default='EN'
+    )
+
+    STATUS_CHOICES = (
+        ('SI', 'Single'),
+        ('MNK', 'Married with No Kids'),
+        ('MNSK', 'Married with No School Kids'),
+        ('MSK', 'Married with School Kids'),
+    )
+    status = models.CharField(
+        max_length=4, choices=STATUS_CHOICES, default='SI'
+    )
+    current_rent = models.FloatField(null=True)
+
+    HOME_TYPE_CHOICES = (
+        ('SF', 'Single Family'),
+        ('TH', 'Townhome'),
+        ('CN', 'Condonomiums'),
+        ('O', 'Other Options')
+    )
+    home_type = models.CharField(
+        max_length=2, choices=HOME_TYPE_CHOICES, blank=True
+    )
+    budget = models.FloatField(blank=True, null=True)
+    annual_income = models.FloatField(blank=True, null=True)
+
+    CREDIT_SCORE_CHOICES = (
+        ('780+', '780+'),
+        ('740-779', '740-779'),
+        ('700-739', '700-739'),
+        ('660-699', '600-699'),
+        ('600-659', '600-659'),
+        ('<599', '<599')
+    )
+    credit_score = models.CharField(
+        max_length=10, choices=CREDIT_SCORE_CHOICES, blank=True, null=True
+    )
+    interested_cities = models.ForeignKey(
+        City, related_name='interested_users', null=True
+    )
 
     def __str__(self):
         return self.username
