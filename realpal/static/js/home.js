cfd73dadacfb56c47621262a8879a5f507c17c47
Harish Maiya <harish.maiya@workday.com>
2017-07-24 00:13:43 -0700

    Putting back base template based structure and minor modifications

--- realpal/static/js/home.js
+++ realpal/static/js/home.js
@@ -1,40 +1,40 @@
 /*global $, jQuery, alert */
 
 $(document).ready(function () {
-    
+
     $('.header .row').height($(window).height());
-    
+
     $(window).resize(function(){
-        
+
     $('.header .row').height($(window).height());
     });
-    
+
    $('.header .row .prepare').mouseover(function(){
        $('.prepare a').children('.works').stop().slideDown()
    });
-    
+
     $('.header .row .prepare').mouseout(function(){
        $('.prepare a').children('.works').stop().slideUp()
    });
-    
-   $('.header .row .buy').mouseover(function(){
-       $('.buy a').children('.works').stop().slideDown()
+
+   $('.header .row .purchase').mouseover(function(){
+       $('.purchase a').children('.works').stop().slideDown()
    });
-    
-    $('.header .row .buy').mouseout(function(){
-       $('.buy a').children('.works').stop().slideUp()
+
+    $('.header .row .purchase').mouseout(function(){
+       $('.purchase a').children('.works').stop().slideUp()
    });
-    
-   $('.header .row .live').mouseover(function(){
-       $('.live a').children('.works').stop().slideDown()
+
+   $('.header .row .own').mouseover(function(){
+       $('.own a').children('.works').stop().slideDown()
    });
-    
-    $('.header .row .live').mouseout(function(){
-       $('.live a').children('.works').stop().slideUp()
+
+    $('.header .row .own').mouseout(function(){
+       $('.own a').children('.works').stop().slideUp()
    });
-   
-   
-    
-       
-    
+
+
+
+
+
 });
