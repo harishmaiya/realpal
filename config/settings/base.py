da700c86f7cf214dd4945bc28e4698f4ce2ecbdc
Udit <udit.wal@gmail.com>
2017-07-31 22:29:56 +0530

    Changing DB settings again

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,20 +110,10 @@ MANAGERS = ADMINS
 # DATABASE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
-# DATABASES = {
-#    'default': env.db('DATABASE_URL', default='postgres:///realpal'),
-# }
-# DATABASES['default']['ATOMIC_REQUESTS'] = True
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('RDS_DB_NAME'),
-        'USER': env('RDS_USERNAME'),
-        'PASSWORD': env('RDS_PASSWORD'),
-        'HOST': env('RDS_HOSTNAME'),
-        'PORT': env('RDS_PORT'),
-    }
+   'default': env.db('DATABASE_URL', default='postgres:///realpal'),
 }
+DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 
 # GENERAL CONFIGURATION
