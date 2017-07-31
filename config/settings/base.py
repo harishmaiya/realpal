9b20720e990da1de44c99f13ef049c1108137c39
Udit <udit.wal@gmail.com>
2017-08-01 02:10:19 +0530

    Commited App Again

--- config/settings/base.py
+++ config/settings/base.py
@@ -57,7 +57,7 @@ LOCAL_APPS = [
     # custom users app
     'realpal.users.apps.UsersConfig',
     # Your stuff: custom apps go here
-    'realpal.mainapp'
+    'realpal.mainapp.apps.MainappConfig'
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
