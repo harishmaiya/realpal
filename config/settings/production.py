dc240229088dddb8348ab7792bfb0d722de4f0e2
Harish Maiya <harish.maiya@workday.com>
2017-08-08 01:58:32 -0700

    changing allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -45,7 +45,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.realpal.io', '.realpal-prod.herokuapp.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.realpal.io', 'realpal-prod.herokuapp.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
