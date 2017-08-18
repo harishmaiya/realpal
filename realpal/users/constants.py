2a163461717db99c5161803ec20be382e7240942
jayden <tjscorp@gmail.com>
2017-08-18 12:44:56 +0200

    moved constants

---
+++ realpal/users/constants.py
@@ -0,0 +1,67 @@
+PURCHASE_STEP_CHOICES = (
+        ('DAP', 'Decide and Prepare'),
+        ('EAO', 'Evaluate and Offer'),
+        ('HOB', 'Home owner benefits'),
+    )
+
+STATUS_CHOICES = (
+        ('SI', 'Single'),
+        ('MNK', 'Married with No Kids'),
+        ('MNSK', 'Married with No School Kids'),
+        ('MSK', 'Married with School Kids'),
+        ('INV', 'Investor')
+    )
+
+HOUSE_TYPE_CHOICES = (
+        ('SF', 'Single Family'),
+        ('TH', 'Townhome'),
+        ('CN', 'Condominium'),
+        ('NC', 'New Construction'),
+        ('OT', 'Other Options'),
+        ('FX', 'Flexible')
+    )
+
+HOUSE_AGE_CHOICES = (
+        ('NC', 'New Construction'),
+        ('15', 'One to Fifteen'),
+        ('30', 'Fifteen to thirty'),
+        ('OLD', 'Over thirty')
+    )
+
+HOUSE_CONDITION_CHOICES = (
+        ('UP', 'Updated'),
+        ('SL', 'Slightly dated'),
+        ('FU', 'Fixer Upper')
+    )
+
+NEIGHBORHOOD_TYPE = (
+        ('HD', 'High Density'),
+        ('MD', 'Medium Density'),
+        ('LD', 'Low Density'),
+        ('AW', 'Anywhere')
+    )
+
+HOW_SOON_CHOICES = (
+        ('3', '0-3 Months'),
+        ('6', '3-6 Months'),
+        ('12', '6-12 Months'),
+        ('12+', '12+ Months'),
+        ('SK', 'Skip')
+    )
+
+LANGUAGE_CHOICES = (
+        ('EN', 'English'),
+        ('ESP', 'Spanish'),
+        ('CA', 'Mandarin/Cantonese'),
+        ('HI', 'Hindi'),
+        ('OT', 'Other'),
+    )
+
+CREDIT_SCORE_CHOICES = (
+        ('780+', '780+'),
+        ('740-779', '740-779'),
+        ('700-739', '700-739'),
+        ('650-699', '650-699'),
+        ('600-649', '600-649'),
+        ('<599', '<599')
+    )
--- realpal/users/models.py
+++ realpal/users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator
+from .constants import PURCHASE_STEP_CHOICES, STATUS_CHOICES, CREDIT_SCORE_CHOICES, HOUSE_AGE_CHOICES, \
+    HOUSE_CONDITION_CHOICES, HOUSE_TYPE_CHOICES, HOW_SOON_CHOICES, LANGUAGE_CHOICES, NEIGHBORHOOD_TYPE
 
 
 @python_2_unicode_compatible
@@ -22,65 +24,28 @@ class User(AbstractUser):
 
     annual_income = models.FloatField(blank=True, null=True)
 
-    PURCHASE_STEP_CHOICES = (
-        ('DAP', 'Decide and Prepare'),
-        ('EAO', 'Evaluate and Offer'),
-        ('HOB', 'Home owner benefits'),
-    )
     purchase_step = models.CharField(
         max_length=3, choices=PURCHASE_STEP_CHOICES, default='DAP'
     )
 
-    STATUS_CHOICES = (
-        ('SI', 'Single'),
-        ('MNK', 'Married with No Kids'),
-        ('MNSK', 'Married with No School Kids'),
-        ('MSK', 'Married with School Kids'),
-        ('INV', 'Investor')
-    )
     status = models.CharField(
         max_length=4, choices=STATUS_CHOICES, default='SI'
     )
 
     firsthome = models.BooleanField(default=True)
 
-    HOUSE_TYPE_CHOICES = (
-        ('SF', 'Single Family'),
-        ('TH', 'Townhome'),
-        ('CN', 'Condominium'),
-        ('NC', 'New Construction'),
-        ('OT', 'Other Options'),
-        ('FX', 'Flexible')
-    )
     house_type = models.CharField(
         max_length=2, choices=HOUSE_TYPE_CHOICES, blank=True
     )
 
-    HOUSE_AGE_CHOICES = (
-        ('NC', 'New Construction'),
-        ('15', 'One to Fifteen'),
-        ('30', 'Fifteen to thirty'),
-        ('OLD', 'Over thirty')
-    )
     house_age = models.CharField(
-        max_length=3, choices=HOUSE_TYPE_CHOICES, blank=True
+        max_length=3, choices=HOUSE_AGE_CHOICES, blank=True
     )
 
-    HOUSE_CONDITION_CHOICES = (
-        ('UP', 'Updated'),
-        ('SL', 'Slightly dated'),
-        ('FU', 'Fixer Upper')
-    )
     house_cond = models.CharField(
         max_length=3, choices=HOUSE_CONDITION_CHOICES, blank=True
     )
 
-    NEIGHBORHOOD_TYPE = (
-        ('HD', 'High Density'),
-        ('MD', 'Medium Density'),
-        ('LD', 'Low Density'),
-        ('AW', 'Anywhere')
-    )
     house_age = models.CharField(
         max_length=2, choices=NEIGHBORHOOD_TYPE, default='AW'
     )
@@ -88,36 +53,12 @@ class User(AbstractUser):
     budget = models.FloatField(blank=True, null=True)
     current_rent = models.FloatField(blank=True, null=True)
 
-    HOW_SOON_CHOICES = (
-        ('3', '0-3 Months'),
-        ('6', '3-6 Months'),
-        ('12', '6-12 Months'),
-        ('12+', '12+ Months'),
-        ('SK', 'Skip')
-    )
     how_soon = models.CharField(max_length=3, choices=HOW_SOON_CHOICES, null=True)
 
-
-
-    LANGUAGE_CHOICES = (
-        ('EN', 'English'),
-        ('ESP', 'Spanish'),
-        ('CA', 'Mandarin/Cantonese'),
-        ('HI', 'Hindi'),
-        ('OT', 'Other'),
-    )
     language = models.CharField(
         max_length=2, choices=LANGUAGE_CHOICES, default='EN'
     )
 
-    CREDIT_SCORE_CHOICES = (
-        ('780+', '780+'),
-        ('740-779', '740-779'),
-        ('700-739', '700-739'),
-        ('650-699', '650-699'),
-        ('600-649', '600-649'),
-        ('<599', '<599')
-    )
     credit_score = models.CharField(
         max_length=10, choices=CREDIT_SCORE_CHOICES, blank=True, null=True
     )
