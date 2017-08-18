00333b888429db93dd3b63b46da14bbb7088be97
jayden <tjscorp@gmail.com>
2017-08-18 13:14:28 +0200

    imported choices

--- realpal/users/constants.py
+++ realpal/users/constants.py
@@ -1,69 +1,127 @@
 AGENT_USER = 0
 CLIENT_USER = 1
-PURCHASE_STEP_CHOICES = (
-        ('DAP', 'Decide and Prepare'),
-        ('EAO', 'Evaluate and Offer'),
-        ('HOB', 'Home owner benefits'),
+
+USER_TYPE_CHOICES = (
+        (AGENT_USER, 'Agent'),
+        (CLIENT_USER, 'Client')
     )
+
+PS_DAP = 0
+PS_EAO = 1
+PS_HOB = 2
+
+PURCHASE_STEP_CHOICES = (
+        (PS_DAP, 'Decide and Prepare'),
+        (PS_EAO, 'Evaluate and Offer'),
+        (PS_HOB, 'Home owner benefits'),
+    )
+
+SC_SI = 0
+SC_MNK = 1
+SC_MNSK = 2
+SC_MSK = 3
+SC_INV = 4
 
 STATUS_CHOICES = (
-        ('SI', 'Single'),
-        ('MNK', 'Married with No Kids'),
-        ('MNSK', 'Married with No School Kids'),
-        ('MSK', 'Married with School Kids'),
-        ('INV', 'Investor')
+        (SC_SI, 'Single'),
+        (SC_MNK, 'Married with No Kids'),
+        (SC_MNSK, 'Married with No School Kids'),
+        (SC_MSK, 'Married with School Kids'),
+        (SC_INV, 'Investor')
     )
+
+HT_SF = 0
+HT_TH = 1
+HT_CN = 2
+HT_NC = 3
+HT_OT = 4
+HT_FX = 5
 
 HOUSE_TYPE_CHOICES = (
-        ('SF', 'Single Family'),
-        ('TH', 'Townhome'),
-        ('CN', 'Condominium'),
-        ('NC', 'New Construction'),
-        ('OT', 'Other Options'),
-        ('FX', 'Flexible')
+        (HT_SF, 'Single Family'),
+        (HT_TH, 'Townhome'),
+        (HT_CN, 'Condominium'),
+        (HT_NC, 'New Construction'),
+        (HT_OT, 'Other Options'),
+        (HT_FX, 'Flexible')
     )
+
+HA_NC = 0
+HA_15 = 1
+HA_30 = 2
+HA_OLD = 3
 
 HOUSE_AGE_CHOICES = (
-        ('NC', 'New Construction'),
-        ('15', 'One to Fifteen'),
-        ('30', 'Fifteen to thirty'),
-        ('OLD', 'Over thirty')
+        (HA_NC, 'New Construction'),
+        (HA_15, 'One to Fifteen'),
+        (HA_30, 'Fifteen to thirty'),
+        (HA_OLD, 'Over thirty')
     )
+
+HC_UP = 0
+HC_SL = 1
+HC_FU = 2
 
 HOUSE_CONDITION_CHOICES = (
-        ('UP', 'Updated'),
-        ('SL', 'Slightly dated'),
-        ('FU', 'Fixer Upper')
+        (HC_UP, 'Updated'),
+        (HC_SL, 'Slightly dated'),
+        (HC_FU, 'Fixer Upper')
     )
+
+NT_HD = 0
+NT_MD = 1
+NT_LD = 2
+NT_AW = 3
 
 NEIGHBORHOOD_TYPE = (
-        ('HD', 'High Density'),
-        ('MD', 'Medium Density'),
-        ('LD', 'Low Density'),
-        ('AW', 'Anywhere')
+        (NT_HD, 'High Density'),
+        (NT_MD, 'Medium Density'),
+        (NT_LD, 'Low Density'),
+        (NT_AW, 'Anywhere')
     )
+
+HS_3 = 0
+HS_6 = 1
+HS_12 = 2
+HS_12P = 3
+HS_SK = 4
 
 HOW_SOON_CHOICES = (
-        ('3', '0-3 Months'),
-        ('6', '3-6 Months'),
-        ('12', '6-12 Months'),
-        ('12+', '12+ Months'),
-        ('SK', 'Skip')
+        (HS_3, '0-3 Months'),
+        (HS_6, '3-6 Months'),
+        (HS_12, '6-12 Months'),
+        (HS_12P, '12+ Months'),
+        (HS_SK, 'Skip')
     )
+
+LC_EN = 0
+LC_ESP = 1
+LC_CA = 2
+LC_HI = 3
+LC_OT = 4
+
 
 LANGUAGE_CHOICES = (
-        ('EN', 'English'),
-        ('ESP', 'Spanish'),
-        ('CA', 'Mandarin/Cantonese'),
-        ('HI', 'Hindi'),
-        ('OT', 'Other'),
+        (LC_EN, 'English'),
+        (LC_ESP, 'Spanish'),
+        (LC_CA, 'Mandarin/Cantonese'),
+        (LC_HI, 'Hindi'),
+        (LC_OT, 'Other'),
     )
 
+CS_780P = 0
+CS_740P = 1
+CS_700P = 2
+CS_650P = 3
+CS_600P = 4
+CS_599P = 5
+
+
 CREDIT_SCORE_CHOICES = (
-        ('780+', '780+'),
-        ('740-779', '740-779'),
-        ('700-739', '700-739'),
-        ('650-699', '650-699'),
-        ('600-649', '600-649'),
-        ('<599', '<599')
+        (CS_780P, '780+'),
+        (CS_740P, '740-779'),
+        (CS_700P, '700-739'),
+        (CS_650P, '650-699'),
+        (CS_600P, '600-649'),
+        (CS_599P, '<599')
     )
--- realpal/users/models.py
+++ realpal/users/models.py
@@ -4,9 +4,6 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator
-from .constants import PURCHASE_STEP_CHOICES, STATUS_CHOICES, CREDIT_SCORE_CHOICES, HOUSE_AGE_CHOICES, \
-    HOUSE_CONDITION_CHOICES, HOUSE_TYPE_CHOICES, HOW_SOON_CHOICES, LANGUAGE_CHOICES, NEIGHBORHOOD_TYPE
-
 from realpal.users.constants import *
 
 
@@ -15,10 +12,7 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    USER_TYPE_CHOICES = (
-        (AGENT_USER, 'Agent'),
-        (CLIENT_USER, 'Client')
-    )
+
     user_type = models.SmallIntegerField(choices=USER_TYPE_CHOICES, default=CLIENT_USER)
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
     zipcode = models.CharField(max_length=10, blank=True, null=True)
@@ -53,17 +47,13 @@ class User(AbstractUser):
         max_length=3, choices=HOUSE_CONDITION_CHOICES, blank=True
     )
 
-    house_age = models.CharField(
-        max_length=2, choices=NEIGHBORHOOD_TYPE, default='AW'
-    )
-
     budget = models.FloatField(blank=True, null=True)
     current_rent = models.FloatField(blank=True, null=True)
 
     how_soon = models.CharField(max_length=3, choices=HOW_SOON_CHOICES, null=True)
 
     language = models.CharField(
-        max_length=2, choices=LANGUAGE_CHOICES, default='EN'
+        max_length=2, choices=LANGUAGE_CHOICES, default=LANGUAGE_CHOICES[LC_EN]
     )
 
     credit_score = models.CharField(
