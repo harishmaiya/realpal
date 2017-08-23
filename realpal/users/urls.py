0192c27d360b0268f788f19e8f5bde296b3bcd06
jayden <tjscorp@gmail.com>
2017-08-23 13:53:52 +0200

    managed to pass all forms to template as context

--- realpal/users/urls.py
+++ realpal/users/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from .models import User
+from realpal.apps.registration.forms import PurchaseStepForm, MaritalStatusForm, FirstHomeForm, HouseTypeForm, \
+    HouseAgeForm, HouseConditionForm, CityForm, MaxBudgetForm, CurrentRentForm, HowSoonForm, PersonalProfileForm
 from .views import UserUpdateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home/home.html'), name='home'),
-    url(r'^messenger$',TemplateView.as_view(template_name='users/messenger.html'), name='messenger'),
-    url(r'^profile/edit$', view=UserUpdateView.as_view(), name='user-update'),
-
+    url(r'^messenger$', TemplateView.as_view(template_name='users/messenger.html'), name='messenger'),
+    url(r'^edit$', view=UserUpdateView.as_view(), name='edit-purchase-step'),
 ]
--- realpal/users/views.py
+++ realpal/users/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import HttpResponse, reverse
+from django.shortcuts import HttpResponse, reverse, get_object_or_404
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import User
 
-from .forms import UpdatePersonalInfoForm
+from realpal.apps.registration.forms import PurchaseStepForm, MaritalStatusForm, FirstHomeForm, HouseTypeForm, \
+    HouseAgeForm, HouseConditionForm, CityForm, MaxBudgetForm, CurrentRentForm, HowSoonForm, PersonalProfileForm
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
@@ -25,14 +26,51 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
-    form_class = UpdatePersonalInfoForm
     template_name = 'users/update.html'
 
-    def get_success_url(self):
-        return HttpResponse('Profile Updated successfully')
+    form_class = PurchaseStepForm  # this will be the default form that is submitted by default
+    forms = {
+        'marital_status_form': MaritalStatusForm,
+        'first_home_form': FirstHomeForm,
+        'house_type_form': HouseTypeForm,
+        'house_age_form': HouseAgeForm,
+        'House_condition_form': HouseConditionForm,
+        'city_form': CityForm,
+        'max_budget_form': MaxBudgetForm,
+        'current_rent_form': CurrentRentForm,
+        'how_soon_form': HowSoonForm,
+        'personal_profile_form': PersonalProfileForm,
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        for form in self.forms:
+            context[form] = self.forms[form]
+        return context
 
     def get_object(self):
         return self.request.user
+
+    def form_invalid(self, **kwargs):
+        return self.render_to_response(self.get_context_data(**kwargs))
+
+    def post(self, request, *args, **kwargs):
+        for form in self.forms:
+            if form in request.POST:
+                form_class = self.get_form_class()
+                form_name = form
+                break
+
+        form = self.get_form(form_class)
+
+        if form.is_valid():
+            form.save()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(**{form_name: form})
+
+    def get_success_url(self):
+        return HttpResponse('Profile Updated successfully')
 
 
 class UserListView(LoginRequiredMixin, ListView):
