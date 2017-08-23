3de9a9a5e796d9f19ac17bc7e4e6e50a2b962673
bornie21 <matembudzeb@gmail.com>
2017-08-23 02:54:46 +0200

    Added reconnect library Added room as named parameter Added login_required to chat room view

--- realpal/apps/chat/consumers.py
+++ realpal/apps/chat/consumers.py
@@ -1,23 +1,77 @@
-from channels.auth import channel_session_user_from_http, channel_session_user
-from channels import Group
 import json
 import logging
-from realpal.apps.chat.models import Room, Message
 
+from channels.auth import channel_session_user_from_http, channel_session_user
+from channels import Group
+from django.utils import timezone
+
+from realpal.apps.chat.models import Room, Message
+from realpal.users.constants import AGENT_USER, CLIENT_USER
 
 logger = logging.getLogger(__name__)
 
 
 @channel_session_user_from_http
-def ws_connect(message):
-    pass
+def ws_connect(message, room_id):
+    message.reply_channel.send({"accept": True})
+    user = message.user
+    if user.is_authenticated:
+        message.channel_session["username"] = user.username
+        try:
+            room = Room.objects.get(pk=room_id)
+            group_name = "Room_{}".format(room.id)
+            message.channel_session["room_id"] = room.id
+            message.channel_session["group_name"] = group_name
+            if user.user_type == AGENT_USER:
+                if room.agent is None:
+                    room.agent = user
+                    room.save()
+                    Group(group_name).add(message.reply_channel)
+                elif room.agent == user:
+                    Group(group_name).add(message.reply_channel)
+                else:
+                    logger.debug('Access denied for user {} on room {}'.format(user, room))
+                    return
+            elif user.user_type == CLIENT_USER:
+                if room.client == user:
+                    Group(group_name).add(message.reply_channel)
+                else:
+                    logger.debug('Access denied for user {} on room {}'.format(user, room))
+                    return
+        except Room.DoesNotExist:
+            print('Requested room not found')
+            logger.debug('Requested room not found')
+            return
+    else:
+        print('Unauthenticated user')
+        logger.debug('Unauthenticated user')
+        return
 
 
 @channel_session_user
 def ws_receive(message):
-    pass
+    incoming = json.loads(message['text'])
+    handle = message.channel_session["username"]
+    msg = incoming.get('message', 'Error Getting Message')
+
+    logger.debug('Incoming message: ', handle, 'msg:', msg)
+
+    data = {
+        'timestamp': timezone.now().strftime('%c'),
+        'handle': handle if handle else 'Anonymous',
+        'message': msg
+    }
+
+    Group(message.channel_session["group_name"]).send({
+        "text": json.dumps({
+            "text": data
+        }),
+    })
 
 
 @channel_session_user
 def ws_disconnect(message):
-    pass
+    try:
+        Group(message.channel_session["group_name"]).discard(message.reply_channel)
+    except KeyError:
+        pass
--- realpal/apps/chat/urls.py
+++ realpal/apps/chat/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from .views import ChatRoomView
+
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'chat/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'},  name='logout'),
+    url(r'^room/$', ChatRoomView.as_view(), name='chat-room'),
+
 ]
