870e41467b737c17ecbd8fd595e8e6b7524f9a5a
jayden <tjscorp@gmail.com>
2017-08-24 21:03:42 +0200

    fixed detail url

--- realpal/apps/users/urls.py
+++ realpal/apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import UserUpdateView, UserListView
+from .views import UserUpdateView, UserListView, UserDetailView
 
 urlpatterns = [
     url(r'^$', UserListView.as_view(), name='list'),
     url(r'^messenger$', TemplateView.as_view(template_name='users/messenger.html'), name='messenger'),
     url(r'^edit$', view=UserUpdateView.as_view(), name='edit'),
+    url(r'^(?P<username>.*)/$', view=UserDetailView.as_view(), name='detail'),
 ]
