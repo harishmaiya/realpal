050077c00b7be9148370ab12ba022fe3479dd3ad
bornie21 <matembudzeb@gmail.com>
2017-09-06 15:55:27 +0200

    Add update room api Add update room api tests Add frontend ajax to unassign client Redirect to chats home if removed client was the currently opened window

--- realpal/apps/users/tests/test_views.py
+++ realpal/apps/users/tests/test_views.py
@@ -81,7 +81,7 @@ class TestUserUpdateView(BaseUserTestCase):
         }
 
         # let's login first since this view is only reachable after login
-        self.client.login(username='testuser', password='password')
+        self.client_user.login(username='testuser', password='password')
 
         # test to see if we reached the user update profile page
         self.assertTemplateUsed('users/update.html')
@@ -89,40 +89,40 @@ class TestUserUpdateView(BaseUserTestCase):
         for form in data:
             data_to_pass = data[form]
             data[form][form] = 'Update'
-            response = self.client.post(update_url, data_to_pass)
+            response = self.client_user.post(update_url, data_to_pass)
             self.assertEqual(response.status_code, 302)
             self.assertTemplateUsed('users/update.html')
 
         # test to see that trying to update with incorrect data will never save the new data
         data = {'purchase_step': 8}  # 8 is not a valid option
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().purchase_step, PS_DAP)  # the default
 
         # testing marital status update
         data = {'status': 8}  # 8 is not a valid option
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().status, None)
 
         # testing house type update
         data = {'house_type': 8, 'house_age': 8, 'house_cond': 8}  # 8 is not a valid option
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().house_type, None)
         self.assertEqual(self.view.get_object().house_age, None)
         self.assertEqual(self.view.get_object().house_cond, None)
 
         # testing budget update
         data = {'budget': 'TEXT'}  # 8 is not a valid option
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().budget, None)
 
         # testing current rent update
         data = {'current_rent': 'TEXT'}  # TEXT is not a valid number
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().current_rent, None)
 
         # testing how soon update
         data = {'how_soon': 8}  # 8 is not a valid option
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().how_soon, None)
 
         # testing profile update
@@ -133,7 +133,7 @@ class TestUserUpdateView(BaseUserTestCase):
                    'phone_number': '+26334465657456774567',  # number too long
                    'email': 'test_email@gmail.com',
                }
-        self.client.post(update_url, data)
+        self.client_user.post(update_url, data)
         self.assertEqual(self.view.get_object().first_name, '')
         self.assertEqual(self.view.get_object().zipcode, None)
         self.assertEqual(self.view.get_object().email, 'testuser')
