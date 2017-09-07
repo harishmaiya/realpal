e6e866099dd5e11df7ae8e5c8fde806be889256c
bornie21 <matembudzeb@gmail.com>
2017-09-07 10:32:55 +0200

    Add relative paths for CSS background images

--- realpal/apps/chat/api.py
+++ realpal/apps/chat/api.py
@@ -1,7 +1,6 @@
 import logging
 import json
 import os
-from urllib.parse import urlparse
 
 from channels import Group
 from rest_framework.generics import CreateAPIView, UpdateAPIView
@@ -63,9 +62,10 @@ class MessageCreateAPIView(CreateAPIView):
                 'user_handle': self.request.user.full_name,
                 'user_type': self.request.user.user_type,
                 'message': instance.text,
-                'file_name': os.path.basename(urlparse(instance.attachment.path).path) if instance.attachment else None,
+                'file_name': os.path.basename(instance.attachment.name) if instance.attachment else None,
                 'file_link': instance.file_download_link if instance.attachment else None,
             }
+            logger.debug('SAVED MESSAGE DATA OBJECT {}'.format(data))
             group_channel = get_room_group_channel(instance.room.id)
             self.push_socket_update(group_channel, data)
 
