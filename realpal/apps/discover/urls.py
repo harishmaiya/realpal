50d443fe1e3491fb01604240affd77955138e42a
jayden <tjscorp@gmail.com>
2017-09-16 20:30:11 +0200

    changed discover layout, added dummy urls

--- realpal/apps/discover/urls.py
+++ realpal/apps/discover/urls.py
@@ -4,4 +4,10 @@ from realpal.apps.discover.views import DiscoverView
 
 urlpatterns = [
     url(r'^$', DiscoverView.as_view(), name='latest'),
+    url(r'^rent-buy$', DiscoverView.as_view(), name='rent-buy'),
+    url(r'^personal-fincance$', DiscoverView.as_view(), name='personal-finance'),
+    url(r'^house-select$', DiscoverView.as_view(), name='house-select'),
+    url(r'^house-loan$', DiscoverView.as_view(), name='house-loan'),
+    url(r'^location$', DiscoverView.as_view(), name='location'),
+    url(r'^agent-selection$', DiscoverView.as_view(), name='agent-selection'),
 ]
