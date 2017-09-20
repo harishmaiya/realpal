cb8ea76c53f7d4dc185d52ac2a97887a1e02a95f
jayden <tjscorp@gmail.com>
2017-09-20 08:30:52 +0200

    switching tabs depending on url

--- realpal/apps/discover/views.py
+++ realpal/apps/discover/views.py
@@ -6,5 +6,5 @@ class DiscoverView(View):
     template_name = 'discover/discover.html'
 
     def get(self, request, *args, **kwargs):
-        url_name = request.GET.get('url_name', '')
+        url_name = kwargs.get('url_name', '').replace('/', '')
         return render(request, self.template_name, context={'url_name': url_name}, status=200)
