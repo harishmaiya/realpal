1b020ea908cccf400683182992053670e1756650
Harish Maiya <harish.maiya@workday.com>
2017-09-22 20:45:13 -0700

    alpha release. changes round 1

--- realpal/apps/discover/urls.py
+++ realpal/apps/discover/urls.py
@@ -5,11 +5,14 @@ from realpal.apps.discover.views import DiscoverView
 urlpatterns = [
     url(r'^$', DiscoverView.as_view(), name='latest'),
     url(r'^rent-buy$', DiscoverView.as_view(), name='rent-buy'),
-    url(r'^personal-fincance$', DiscoverView.as_view(), name='personal-finance'),
+    url(r'^personal-finance$', DiscoverView.as_view(), name='personal-finance'),
     url(r'^house-select$', DiscoverView.as_view(), name='house-select'),
     url(r'^house-loan$', DiscoverView.as_view(), name='house-loan'),
     url(r'^location$', DiscoverView.as_view(), name='location'),
     url(r'^agent-selection$', DiscoverView.as_view(), name='agent-selection'),
     url(r'^your-journey', DiscoverView.as_view(), name='your-journey'),
     url(r'^(?P<url_name>.+)$', DiscoverView.as_view(), name='param'),
+    url(r'^trends', DiscoverView.as_view(), name='trends'),
+    url(r'^media', DiscoverView.as_view(), name='media'),
+    url(r'^stories', DiscoverView.as_view(), name='stories'),
 ]
